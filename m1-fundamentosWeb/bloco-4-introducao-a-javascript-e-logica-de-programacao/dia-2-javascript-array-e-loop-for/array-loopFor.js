a80dc00d549dc59fdcf8e5bbffe8851d9ac34dd5
EDUARDO FERREIRA <dupearljam@gmail.com>
2021-11-29 18:59:02 -0300

    exercicio 1

---
+++ m1-fundamentosWeb/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-2-javascript-array-e-loop-for/array-loopFor.js
@@ -0,0 +1,12 @@
+// ** Exercício 1 - imprimindo os números do array
+
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+
+for (let i = 0; i < numbers.length; i += 1) {
+    console.log('O número',numbers[i],'é o',i+1,'º número do array.' );
+}
+
+
+
+
+
