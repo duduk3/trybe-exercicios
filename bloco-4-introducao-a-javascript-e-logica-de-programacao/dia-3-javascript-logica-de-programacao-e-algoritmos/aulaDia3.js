4e309c9ac2c3c7e999d37b43f964aa434292d50a
EDUARDO FERREIRA <dupearljam@gmail.com>
2021-11-30 18:44:37 -0300

    exercicio 1 do dia 3 de javascript

---
+++ bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-3-javascript-logica-de-programacao-e-algoritmos/aulaDia3.js
@@ -0,0 +1,12 @@
+//** */ Exercício 1 - criar um quadrado de asterisco
+
+let n = 5;
+let asterisco = '';
+
+for ( let i = 1; i <= n; i += 1 ) {
+    asterisco += '*';
+}
+
+for ( let i = 1; i <= n; i += 1 ) {
+    console.log(asterisco);
+}
