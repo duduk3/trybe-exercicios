8f5cf751167d4d889d764158d4d22b255ebb8dd0
EDUARDO FERREIRA <dupearljam@gmail.com>
2021-11-30 18:46:54 -0300

    exercicio 2 do dia 3 de javascript

--- bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-3-javascript-logica-de-programacao-e-algoritmos/aulaDia3.js
+++ bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-3-javascript-logica-de-programacao-e-algoritmos/aulaDia3.js
@@ -10,3 +10,16 @@ for ( let i = 1; i <= n; i += 1 ) {
 for ( let i = 1; i <= n; i += 1 ) {
     console.log(asterisco);
 }
+
+
+//** */ Exercício 2 - criar um triângulo retângulo de asterisco
+
+n = 5;
+asterisco = '';
+
+for ( let i = 1; i <= n; i += 1 ) {
+    asterisco += '*';
+    console.log(asterisco);
+}
+
+
