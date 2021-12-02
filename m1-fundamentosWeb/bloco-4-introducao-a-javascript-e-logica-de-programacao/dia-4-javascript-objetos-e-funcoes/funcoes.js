9c3cbc7c4d3d1dc5dbc9ff8b465b6da308a9d628
EDUARDO FERREIRA <dupearljam@gmail.com>
2021-12-02 06:16:38 -0300

    Exercicio 1 do dia 4 do bloco 4 - concluido

--- m1-fundamentosWeb/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-4-javascript-objetos-e-funcoes/funcoes.js
+++ m1-fundamentosWeb/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-4-javascript-objetos-e-funcoes/funcoes.js
@@ -1,19 +1,22 @@
 //**Exercício 1 do dia 4 do blobo 4 */
 
-function palindromo(word) {
+function verificaPalindrome(word) {
 
     var tamanho = word.length;
     var meio = Math.floor(tamanho/2);
 
     for ( var i = 0; i < meio; i++ ) {
         if (word[i] !== word[tamanho - 1 - i]) {
-            return console.log('Não é um palíndromo');
+            return  console.log('false');
         }
     }
 
-    return console.log('É um palíndromo');
+    return console.log('true');
 }
-palindromo('arara');
+
+verificaPalindrome('arara');
+
+verificaPalindrome('desenvolvimento');
 
 
 
