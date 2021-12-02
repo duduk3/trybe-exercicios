be46d1631b4fd7c8bdaca7e491ae87c0a8562f1f
EDUARDO FERREIRA <dupearljam@gmail.com>
2021-12-02 06:44:02 -0300

    Exercicio 2 do dia 4 do bloco 4 - concluido

--- m1-fundamentosWeb/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-4-javascript-objetos-e-funcoes/funcoes.js
+++ m1-fundamentosWeb/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-4-javascript-objetos-e-funcoes/funcoes.js
@@ -22,6 +22,18 @@ verificaPalindrome('desenvolvimento');
 
 //**Exercício 2 do dia 4 do blobo 4 */
 
+let maxArray = [2, 3, 6, 7, 10, 1];
+let max = 0;
 
+function getMaxIndex (array){
+    for ( let i = 0; i < maxArray.length; i += 1) {
+        if (maxArray[i] > max){
+            max = maxArray[i];
+        }
+    }
+    console.log(max); 
+}
+
+getMaxIndex(maxArray)
 
 
