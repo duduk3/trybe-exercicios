6e3cee72bec3d5bb20610e86667c430c9c527d68
EDUARDO FERREIRA <dupearljam@gmail.com>
2021-12-03 18:16:26 -0300

    modifica funçao mas ainda não conclui o exercício

--- m1-fundamentosWeb/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-4-javascript-objetos-e-funcoes/funcoes.js
+++ m1-fundamentosWeb/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-4-javascript-objetos-e-funcoes/funcoes.js
@@ -1,4 +1,6 @@
+//===================================================
 //**Exercício 1 do dia 4 do blobo 4 */
+//===================================================
 
 function verificaPalindrome(word) {
 
@@ -19,8 +21,9 @@ verificaPalindrome('arara');
 verificaPalindrome('desenvolvimento');
 
 
-
+//===================================================
 //**Exercício 2 do dia 4 do blobo 4 */
+//===================================================
 
 let maxArray = [2, 3, 6, 7, 10, 1];
 let max = 0;
@@ -37,8 +40,9 @@ function getMaxIndex (array){
 getMaxIndex(maxArray);
 
 
-
+//===================================================
 //**Exercício 3 do dia 4 do blobo 4 */
+//===================================================
 
 let minArray = [2, 4, 6, 7, 10, 0, -3];
 let min = minArray[0];
@@ -53,4 +57,28 @@ function getMinIndex (array){
 }
 
 getMinIndex(minArray);
+
+
+
+//===================================================
+//**Exercício 4 do dia 4 do blobo 4 */
+//===================================================
+
+let nameArray = ['José', 'Lucas', 'Nádia', 'Fernanda', 'Cairo', 'Joana'];
+max = 0;
+let maxName = '';
+
+function getMaxName (array){
+    for ( let i = 0; i < nameArray.length; i += 1) {
+//        for (let count = 0; count < nameArray[i].length; count += 1);{
+            if (nameArray[i].length > max){
+                maxName = nameArray[i];
+                console.log(nameArray[i].length);
+            }
+ //       } 
+    }
+    console.log(maxName); 
+}
+
+getMaxName(nameArray);
 
