a5e7e0e113e11e0bb4fb773934994c0f41bea6b4
EDUARDO FERREIRA <dupearljam@gmail.com>
2021-12-08 12:09:44 -0300

    exercicio 1 = insere tudo dentro de uma funçao

--- m1-fundamentosWeb/bloco-5-javascript-dom-eventos-e-web-storage/dia-3-javascript-eventos/script.js
+++ m1-fundamentosWeb/bloco-5-javascript-dom-eventos-e-web-storage/dia-3-javascript-eventos/script.js
@@ -14,19 +14,24 @@ function createDaysOfTheWeek() {
   createDaysOfTheWeek();
   
 const dezDaysList = [29, 30, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31];
-let ulDays = document.querySelector('#days');
-let liDay = document.createElement('li');
 
-for (let i = 0; i < dezDaysList.length; i += 1) {
-  ulDays = document.querySelector('#days');
-  liDay = document.createElement('li');
-  liDay.className = 'day';
-  ulDays.appendChild(liDay);
+function addDays () {
+  let ulDays = document.querySelector('#days');
+  let liDay = document.createElement('li');
+  
+  for (let i = 0; i < dezDaysList.length; i += 1) {
+    ulDays = document.querySelector('#days');
+    liDay = document.createElement('li');
+    liDay.className = 'day';
+    ulDays.appendChild(liDay);
+  }
+  
+  let takeLiDay = document.getElementsByClassName('day');
+  
+  for (let index = 0; index < takeLiDay.length; index++) {
+    takeLiDay[index].innerText = dezDaysList[index];
+  }
 }
 
-let takeLiDay = document.getElementsByClassName('day');
-
-for (let index = 0; index < takeLiDay.length; index++) {
-  takeLiDay[index].innerText = dezDaysList[index];
-}
+addDays();
 
