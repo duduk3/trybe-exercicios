fa695c544113fb06f614ffc5cff291e62906912a
EDUARDO FERREIRA <dupearljam@gmail.com>
2021-12-08 19:22:28 -0300

    atualiza para adicionar o exercício do dia

--- m1-fundamentosWeb/bloco-5-javascript-dom-eventos-e-web-storage/dia-3-javascript-eventos/script.js
+++ m1-fundamentosWeb/bloco-5-javascript-dom-eventos-e-web-storage/dia-3-javascript-eventos/script.js
@@ -12,7 +12,10 @@ function createDaysOfTheWeek() {
   };
   
   createDaysOfTheWeek();
-  
+
+//* ================================================================================
+//* Exercício 1
+//* ================================================================================
 const dezDaysList = [29, 30, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31];
 
 function addDays () {
@@ -35,3 +38,21 @@ function addDays () {
 
 addDays();
 
+//* Exercício 2
+//* ================================================================================
+function addFeriado () {
+  let buttonContainer = document.querySelector(".buttons-container");
+  let buttonFilho = document.createElement('button');
+  buttonFilho.className = "btn-holiday";
+  buttonContainer.appendChild(buttonFilho);
+  document.querySelector('.btn-holiday').innerText = "Feriados";
+}
+
+addFeriado();
+
+//* Exercício 3
+//* ================================================================================
+let takeButtonFeriado = document.querySelector('.btn-holiday');
+let takeHolidays = document.querySelectorAll('.holiday');
+
+console.log(takeHolidays);
