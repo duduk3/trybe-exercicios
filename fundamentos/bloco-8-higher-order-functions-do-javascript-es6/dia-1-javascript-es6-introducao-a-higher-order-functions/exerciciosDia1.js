8627459b26db234050ca976e8d59793e02712ade
EDUARDO FERREIRA <dupearljam@gmail.com>
2022-01-18 20:05:32 -0300

    insere uma funçao do exercicio2

--- fundamentos/bloco-8-higher-order-functions-do-javascript-es6/dia-1-javascript-es6-introducao-a-higher-order-functions/exerciciosDia1.js
+++ fundamentos/bloco-8-higher-order-functions-do-javascript-es6/dia-1-javascript-es6-introducao-a-higher-order-functions/exerciciosDia1.js
@@ -1,18 +1,41 @@
-const newEmployees = (callBack) => {
-    const employees = {
-      id1: callBack('Pedro Guerra'), // Nome: Pedro Guerra -> Chame sua função passando o nome Pedro Guerra como parâmetro, substituindo as aspas
-      id2: callBack('Luiza Drumond'), // Nome: Luiza Drumond -> Chame sua função passando o nome Luiza Drumond como parâmetro, substituindo as aspas
-      id3: callBack('Carla Paiva'), // Nome: Carla Paiva -> Chame sua função passando o nome Carla Paiva como parâmetro, substituindo as aspas
-    }
-    return employees;
-  };
+// //*----======== EXERCÍCIO 1 DO DIA 1 BLOCO 8 =========----*//
+// const newEmployees = (callBack) => {
+//     const employees = {
+//       id1: callBack('Pedro Guerra'), // Nome: Pedro Guerra -> Chame sua função passando o nome Pedro Guerra como parâmetro, substituindo as aspas
+//       id2: callBack('Luiza Drumond'), // Nome: Luiza Drumond -> Chame sua função passando o nome Luiza Drumond como parâmetro, substituindo as aspas
+//       id3: callBack('Carla Paiva'), // Nome: Carla Paiva -> Chame sua função passando o nome Carla Paiva como parâmetro, substituindo as aspas
+//     }
+//     return employees;
+//   };
 
-  const createEmail = (name) => {
-      const newEmail = name.toLowerCase().replace(' ', '_');
-    return {
-        nome: name,
-        email: `${newEmail}@trybe.com`
-    }
-  };
+//   const createEmail = (name) => {
+//       const newEmail = name.toLowerCase().replace(' ', '_');
+//     return {
+//         nome: name,
+//         email: `${newEmail}@trybe.com`
+//     }
+//   };
 
-console.log(newEmployees(createEmail));
+// console.log(newEmployees(createEmail));
+
+//*----======== EXERCÍCIO 2 DO DIA 1 BLOCO 8 =========----*//
+
+const goodLuck = (yourNumber, callBack) => {
+    if ( yourNumber === callBack ) {
+        return "Parabéns você ganhou";
+    } else {
+        return "Tente novamente";s
+    }
+}
+
+
+const sortNum = () => {
+    const luckNum = Math.round(Math.random() * 4) + 1;
+    
+    return luckNum;
+}
+
+console.log(goodLuck(1, sortNum()));
+
+//*----======== EXERCÍCIO 3 DO DIA 1 BLOCO 8 =========----*//
+
