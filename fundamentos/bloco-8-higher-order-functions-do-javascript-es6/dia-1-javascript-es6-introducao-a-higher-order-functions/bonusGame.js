d9ebd2fbf96bde6613762f2f33a3826beaa16e94
EDUARDO FERREIRA <dupearljam@gmail.com>
2022-01-22 13:24:17 -0300

    refatora exercicio mas nao finaliza

--- fundamentos/bloco-8-higher-order-functions-do-javascript-es6/dia-1-javascript-es6-introducao-a-higher-order-functions/bonusGame.js
+++ fundamentos/bloco-8-higher-order-functions-do-javascript-es6/dia-1-javascript-es6-introducao-a-higher-order-functions/bonusGame.js
@@ -5,67 +5,69 @@ const mage = {
     damage: undefined,
   };
   
-  const warrior = {
-    healthPoints: 200,
-    strength: 30,
-    weaponDmg: 2,
-    damage: undefined,
-  };
+const warrior = {
+healthPoints: 200,
+strength: 30,
+weaponDmg: 2,
+damage: undefined,
+};
   
-  const dragon = {
-    healthPoints: 350,
-    strength: 50,
-    damage: undefined,
-  };
+const dragon = {
+healthPoints: 350,
+strength: 50,
+damage: undefined,
+};
   
-    const battleMembers = { mage, warrior, dragon };
+const battleMembers = { mage, warrior, dragon };
 
-    const dragonDamage = () => {
-        dragon.damage = Math.round(Math.random() * dragon.strength) + 15;
-        return dragon.damage;
+const dragonDamage = () => {
+    dragon.damage = Math.round(Math.random() * dragon.strength) + 15;
+    return dragon.damage;
+}
+
+const warriorDamage = () => {
+    warrior.damage = Math.round(Math.random() * warrior.strength * warrior.weaponDmg) + warrior.strength;
+    return warrior.damage;
+}
+
+const mageDamage = () => {
+    mage.damage = Math.round(Math.random() * mage.intelligence * 2) + mage.intelligence;
+    if (mage.mana < 15) {
+        alert('Não possui mana suficiente');
+    } else {
+        mage.mana -= 15;
     }
+    return {
+        dano: mage.damage,
+        mana: mage.mana
+    } 
+};
 
-    const warriorDamage = () => {
-        warrior.damage = Math.round(Math.random() * warrior.strength * warrior.weaponDmg) + warrior.strength;
-        return warrior.damage;
-    }
 
-    const mageDamage = () => {
-        mage.damage = Math.round(Math.random() * mage.intelligence * 2) + mage.intelligence;
-        if (mage.mana < 15) {
-            alert('Não possui mana suficiente');
-        } else {
-            mage.mana -= 15;
-        }
-        return {
-            dano: mage.damage,
-            mana: mage.mana
-        } 
+const warriorTurn = (rodada1) => {
+    let dano1 = rodada1();
+    dragon.healthPoints = dragon.healthPoints - dano1;
+    warrior.damage = dano1;
+};
+
+const mageTurn = (rodada2) => {
+    let dano2 = rodada2();
+    dragon.healthPoints = dragon.healthPoints - dano2;
+    mage.damage = dano2;
+};
+
+const dragonTurn = (rodada3) => {
+    let dano3 = rodada3();
+    mage.healthPoints = mage.healthPoints - dano3;
+    warrior.healthPoints = warrior.healthPoints - dano3;
+    dragon.damage = dano3;
+};
+
+const gameActions = {
+        warriorTurn: warriorTurn(warriorDamage) ,
+        mageTurn: mageTurn(mageDamage) ,
+        dragonTurn: dragonTurn(dragonDamage),
+        atual: console.log(battleMembers)
     };
 
-    const warriorTurn = (warriorDamage) => {
-        battleMembers.dragon.healthPoints - warriorDamage();
-        battleMembers.warrior.damage - warriorDamage();
-    };
 
-    const mageTurn = (mageDamage) => {
-        battleMembers.dragon.healthPoints - mageDamage();
-        battleMembers.mage.damage - mageDamage();
-    };
-
-    const dragonTurn = (dragonDamage) => {
-        battleMembers.mage.healthPoints - dragonDamage();
-        battleMembers.warrior.healthPoints - dragonDamage();
-        battleMembers.dragon.damage - dragonDamage();
-    };
-
-    const gameActions = {
-        // Crie as HOFs neste objeto.
-        newWarriorTurn: () => warriorTurn(),
-        newMageTurn: () => mageTurn(),
-        newDragonTurn: () => dragonTurn(),
-        newBattleMembers: () => console.log(battleMembers)
-      };
-
-      console.log(gameActions.newDragonTurn);
-      console.log(gameActions.newBattleMembers);
