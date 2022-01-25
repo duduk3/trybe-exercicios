d6405d9839c4cc20d2ce3e21e3848073b7ad1a9f
EDUARDO FERREIRA <dupearljam@gmail.com>
2022-01-24 21:36:33 -0300

    resolve exercicio 5

--- fundamentos/bloco-8-higher-order-functions-do-javascript-es6/dia-5-javascript-es6-spread-operator-parametro-rest-destructuring-e-mais/exercise1.js
+++ fundamentos/bloco-8-higher-order-functions-do-javascript-es6/dia-5-javascript-es6-spread-operator-parametro-rest-destructuring-e-mais/exercise1.js
@@ -91,3 +91,12 @@ const filterPeople = (people) => {
 
 const pessoas = filterPeople(people);
 
+
+//* 5 - Escreva a função swap , que dado um array de 3 elementos, retorna um novo array com o primeiro e terceiro elementos trocados. Detalhe: você precisa fazer essa função gastando 1 linha só:
+
+const myList = [1, 2, 3];
+
+const swap = (array) => [array[2], array[1], array[0]];
+
+console.log(swap(myList));
+
