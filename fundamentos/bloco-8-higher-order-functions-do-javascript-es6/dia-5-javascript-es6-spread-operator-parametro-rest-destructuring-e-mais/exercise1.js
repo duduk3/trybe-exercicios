55869a9e3677708c5a50f7eabbaa7ad3114ca52a
EDUARDO FERREIRA <dupearljam@gmail.com>
2022-01-24 21:42:21 -0300

    resolve exercicio 6

--- fundamentos/bloco-8-higher-order-functions-do-javascript-es6/dia-5-javascript-es6-spread-operator-parametro-rest-destructuring-e-mais/exercise1.js
+++ fundamentos/bloco-8-higher-order-functions-do-javascript-es6/dia-5-javascript-es6-spread-operator-parametro-rest-destructuring-e-mais/exercise1.js
@@ -100,3 +100,21 @@ const swap = (array) => [array[2], array[1], array[0]];
 
 console.log(swap(myList));
 
+//* 6 - Suponha que você esteja lidando com carros e, da forma como o problema lhe foi entregue, cada carro é modelado como um array. Porém, essa modelagem está baixo nível. Cria uma função toObject que, dada uma lista, retorna um objeto representando o carro:
+
+const palio = ['Palio', 'Fiat', 2019];
+const shelbyCobra = ['Shelby Cobra', 'Ford', 1963];
+const chiron = ['Chiron', 'Bugatti', 2016];
+
+const toObject = (array) => {
+  return {
+    carro: array[0],
+    marca: array[1],
+    ano: array[2]
+  }
+}
+console.log(toObject(palio));
+console.log(toObject(shelbyCobra));
+console.log(toObject(chiron));
+
+
