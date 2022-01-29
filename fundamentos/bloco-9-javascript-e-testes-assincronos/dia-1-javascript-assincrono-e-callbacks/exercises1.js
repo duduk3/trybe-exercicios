077d7c321f2c2ddf36651c1736373fd87f214862
EDUARDO FERREIRA <dupearljam@gmail.com>
2022-01-28 21:43:34 -0300

    adiciona o codigo do exercicio2

--- fundamentos/bloco-9-javascript-e-testes-assincronos/dia-1-javascript-assincrono-e-callbacks/exercises1.js
+++ fundamentos/bloco-9-javascript-e-testes-assincronos/dia-1-javascript-assincrono-e-callbacks/exercises1.js
@@ -28,3 +28,7 @@ const jupiter = {
 console.log(planetDistanceFromSun(mars)); // A
 console.log(planetDistanceFromSun(venus)); // B
 console.log(planetDistanceFromSun(jupiter)); // C
+
+console.log(planetDistanceFromSun(mars)); // A
+setTimeout(() => console.log(planetDistanceFromSun(venus)), 3000); // B
+setTimeout(() => console.log(planetDistanceFromSun(jupiter)), 2000); // C
