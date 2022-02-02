db8170af4c00028cfc32b002967f08bb652c0ea6
EDUARDO FERREIRA <dupearljam@gmail.com>
2022-02-02 10:48:13 -0300

    finaliza parte1 do exercicio

--- fundamentos/bloco-9-javascript-e-testes-assincronos/dia-2-javascript-assincrono-fetch-api-e-asyncawait/api.js
+++ fundamentos/bloco-9-javascript-e-testes-assincronos/dia-2-javascript-assincrono-fetch-api-e-asyncawait/api.js
@@ -1,25 +1,28 @@
 const API_URL = `https://api.coincap.io/v2/assets`;
 
-let criptoArray;
 
-const fetchCripto = async () => {
 
-    fetch(API_URL)
-        .then((response) => response.json())
-        .then((data) => criptoArray = data.data)
-        .catch((err) => console.log(err))
-        
-    return criptoArray;
+function createElements (current) {
+    const coin = document.createElement('li');
+    coin.innerHTML = `"<span>${ current.name }</span>" - Symbol (<span>${current.symbol}</span>) hoje: custa <span>U$ ${current.priceUsd}</span>.`;
+    coin.className = 'cripto-list'
+    return coin;
 }
 
-// criptoArray = criptoArray.filter((element) => element.rank <= 10).map((elem) => {
-//     return { 
-//         name: elem.name,
-//         symbol: elem.symbol,
-//         price: elem.priceUsd
-//     }
-// });
 
-console.log(criptoArray);
+const fetchCripto = async () => {
+    
+    const response = await fetch(API_URL);
+    const { data } = await response.json();
+ 
+    const filtered10First = data.filter((element) => element.rank <= 10);
+    
+    const coinList = document.querySelector('.cripto-list');
+    filtered10First.forEach((current) => {
+    coinList.appendChild(createElements(current));
+
+    });        
+}
+
 
 window.onload = () => fetchCripto();
